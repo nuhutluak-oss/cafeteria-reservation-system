c4be177c32fd0bb281d54be6507b8d0633cca0e5
nuhutluak-oss <nuhutluak@gmail.com>
2026-01-17 20:18:00 +0300

    Create User.java

---
+++ src/models/User.java
@@ -0,0 +1,9 @@
+public class User {
+    protected int userId;
+    protected String name;
+    protected String email;
+    protected String password;
+
+    public void login() {}
+    public void logout() {}
+}

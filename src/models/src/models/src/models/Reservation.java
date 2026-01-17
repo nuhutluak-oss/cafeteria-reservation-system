37f9828189e27045667530ecfcc151ac3c07e2c0
nuhutluak-oss <nuhutluak@gmail.com>
2026-01-17 20:42:45 +0300

    Create Reservation.java

---
+++ src/models/src/models/src/models/Reservation.java
@@ -0,0 +1,10 @@
+public class Reservation {
+    private int reservationId;
+    private int userId;
+    private String date;
+    private String timeSlot;
+    private String status;
+
+    public void create() {}
+    public void cancel() {}
+}
